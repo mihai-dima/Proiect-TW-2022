fb766bececce346fe3a4a2bc374767fcabef8b62
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-06-12 23:05:49 +0300

    .

--- Main/groups.php
+++ Main/groups.php
@@ -4,6 +4,15 @@
 <?php require "homeBody.php"; ?>
 <!DOCTYPE html>
 <html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <?php include "homeBody.php"; ?>
+
 <div class="container">
   <!------------------left-sidebar------------------->
       <div class="left-sidebar">
@@ -16,8 +25,17 @@
 
     <!------------------main-content------------------->
      <div class="main-content">
-
-     </div>
+       <div class="custom-select">
+           <select>
+             <option value="0">Select domain</option>
+             <option value="1">Music</option>
+             <option value="2">Sport</option>
+             <option value="3">Policy</option>
+             <option value="4">Art</option>
+             <option value="5">Instagram Influencers</option>
+           </select>
+       </div>
+    </div>
     <!-------------------right-sidebar----------------->
      <div class="right-sidebar">
 
