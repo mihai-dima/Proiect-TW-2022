b8b3e5a7315a25534ab9efb669592b77a012deee
mihai-dima <mihai.dima@info.uaic.ro>
2022-06-22 05:39:24 +0300

    .

--- Main/groups.php
+++ Main/groups.php
@@ -10,8 +10,7 @@ session_start();
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>Autograph Collector</title>
   <link rel="stylesheet" href="styleMain.css">
-  <link rel="stylesheet" href="styleSelect.css">
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <link rel="stylesheet" href="styleAutographs.css">
   <script>
     $(document).ready(function() {
       $(".default_optionB").click(function() {
@@ -25,62 +24,18 @@ session_start();
       })
     });
   </script>
+  
 </head>
 
 <body>
   <?php require_once("homeBody.html"); ?>
   <!--main-content-->
   <div class="main-content">
-    <form>
-      <div class="wrapperB">
-        <div class="select_wrapB">
-          <ul class="default_optionB">
-            <li>
-              <div class="optionB default">
-                <p>Select domain</p>
-              </div>
-            </li>
-          </ul>
-          <ul class="select_ulB">
-            <li>
-              <div class="optionB music">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Music';" value="Music">
-              </div>
-            </li>
-            <li>
-              <div class="optionB sport">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Sport';" value="Sport">
-              </div>
-            </li>
-            <li>
-              <div class="optionB politics">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Politics';" value="Politics">
-              </div>
-            </li>
-            <li>
-              <div class="optionB movie">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Movie';" value="Movie">
-              </div>
-            </li>
-            <li>
-              <div class="optionB culture">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Culture';" value="Culture">
-              </div>
-            </li>
-            <li>
-              <div class="optionB science">
-                <input type="button" onclick="window.location.href = '../Main/createTop.php?domain=Science';" value="Science">
-              </div>
-            </li>
-          </ul-->
-        </div>
-      </div>
-    </form>
+    <div id="search-result"> 
+      <p> These are results of your search: </p>
   </div>
-  <!--right-sidebar-->
-  <div class="right-sidebar">
-
   </div>
+ 
   </div>
   <!--container-->
   <script src="script.js"></script>
